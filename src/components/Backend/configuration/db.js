b3cb40372353437b8a41e5c75305e92a786b7a2a
cosmic <140958518+arghya-align-pix-design@users.noreply.github.com>
2025-01-13 11:47:29 +0530

    Update db.js

--- src/components/Backend/configuration/db.js
+++ src/components/Backend/configuration/db.js
@@ -2,9 +2,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const mongo="https://perfumes-atpy.onrender.com";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(mongo, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
